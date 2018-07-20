3b4ba2066ce054c3fc8f420c4cc50cfea0485c67
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-07-20 11:56:41 -0500

    added a for loop to show all pictures. no issues

--- pokemon.js
+++ pokemon.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    
+
+    for(var i=1;i<152;i++){
+        $("body").before("<img src='http://pokeapi.co/media/img/"+i+".png' alt='a picture of a pokemon'>");
+    }
 });
