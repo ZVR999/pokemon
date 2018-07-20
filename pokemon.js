6f116d3b2e62ac9dd8b2f670709632900dc9086f
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-07-20 13:10:17 -0500

    tested .before(),.after().append(), and prepend() on loop

--- pokemon.js
+++ pokemon.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
     for(var i=1;i<152;i++){
-        $("body").before("<img src='http://pokeapi.co/media/img/"+i+".png' alt='a picture of a pokemon'>");
+        $("body").append("<img src='http://pokeapi.co/media/img/"+i+".png' alt='a picture of a pokemon'>");
     }
 });
